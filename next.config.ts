b92b9aeb66e9d98aeb22877c45f01e5d7d21ef68
Júlio Cesar Guimarães <jcguimaraes@gmail.com>
2026-02-13 00:00:36 -0300

    chore(web): use webpack to solve pino package not found

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,27 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "standalone",
-  serverExternalPackages: ["pino", "pino-pretty"],
+  serverExternalPackages: [
+    "pino",
+    "pino-pretty",
+    "pino-abstract-transport",
+    "pino-std-serializers",
+    "thread-stream",
+    "sonic-boom",
+    "on-exit-leak-free",
+  ],
   outputFileTracingIncludes: {
     "/api/copilotkit": [
       "./node_modules/pino/**/*",
       "./node_modules/pino-pretty/**/*",
+      "./node_modules/pino-abstract-transport/**/*",
+      "./node_modules/pino-std-serializers/**/*",
       "./node_modules/thread-stream/**/*",
+      "./node_modules/sonic-boom/**/*",
+      "./node_modules/on-exit-leak-free/**/*",
+      "./node_modules/fast-redact/**/*",
+      "./node_modules/quick-format-unescaped/**/*",
+      "./node_modules/secure-json-parse/**/*",
     ],
   },
 };
